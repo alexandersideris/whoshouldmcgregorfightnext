bd7de427131a2c3cb670a3a5865abe65c72a2e27
alexandersideris <alexander.sideris.west@gmail.com>
2017-12-29 12:28:04 +0200

    Updated seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
 
-Fighter.destroy_all
-Fight.destroy_all
 
 $flyweights = Array.new([])
 $bantamweights = Array.new([])
@@ -13,6 +11,11 @@ $middleweights = Array.new([])
 $lightheavyweights = Array.new([])
 $heavyweights = Array.new([])
 
+$womenstrawweights = Array.new([])
+$womenflyweights = Array.new([])
+$womenbantamweights = Array.new([])
+$womenfeatherweights = Array.new([])
+
 def createFights
 	puts 'Flyweights:' + $flyweights.to_s
 	puts 'Bantamweights:' + $bantamweights.to_s
@@ -22,6 +25,11 @@ def createFights
 	puts 'Middleweights:' + $middleweights.to_s
 	puts 'Light Heavyweights:' + $lightheavyweights.to_s
 	puts 'Heavyweights:' + $heavyweights.to_s
+
+	puts 'Women Strawweights:' + $womenstrawweights.to_s
+	puts 'Women Flyweights:' + $womenflyweights.to_s
+	puts 'Women Bantamweights:' + $womenbantamweights.to_s
+	puts 'Women Featherweights:' + $womenfeatherweights.to_s
 end
 
 def createFighter(division, rank, name, url, rank_number)
@@ -42,9 +50,9 @@ def createFighter(division, rank, name, url, rank_number)
 	str2 = '" alt'
 
 	puts photo_url.to_s[/#{str1}(.*?)#{str2}/m, 1].to_s
-  img_url = 'http://'+photo_url.to_s[/#{str1}(.*?)#{str2}/m, 1].to_s
+  	img_url = 'https://'+photo_url.to_s[/#{str1}(.*?)#{str2}/m, 1].to_s
 
-  fighter_details = parsed_content_two.css('div#content').css('.content-inner').css('div#fighter-details').css('div#fighter-overlay')
+  	fighter_details = parsed_content_two.css('div#content').css('.content-inner').css('div#fighter-details').css('div#fighter-overlay')
 
 	# Fighter's record
 
@@ -55,7 +63,21 @@ def createFighter(division, rank, name, url, rank_number)
 	puts 'Record: ' + record[/#{str1}(.*?)#{str2}/m, 1].to_s
   the_record = record[/#{str1}(.*?)#{str2}/m, 1].to_s
 
-  f = Fighter.create(name: name, division: division, rank: rank, rank_number: rank_number, img_url: img_url, fight_record: the_record)
+	# Check if the fighter already exists in the database. We will pretend that names are unique, which they are atm
+	f = Fighter.where(name: name).take
+	if f != nil
+		f.division = division
+		f.rank = rank
+		f.rank_number = rank_number
+		f.img_url = img_url
+		f.fight_record = the_record
+		f.is_updated = 'true'
+		f.save
+	else
+		f = Fighter.create(name: name, division: division, rank: rank, rank_number: rank_number, img_url: img_url, fight_record: the_record, is_updated: 'true')
+	end
+
+
 	if division == 'Flyweight'
 		$flyweights.push(f)
 	elsif division == 'Bantamweight'
@@ -72,6 +94,54 @@ def createFighter(division, rank, name, url, rank_number)
 		$lightheavyweights.push(f)
 	elsif division == 'Heavyweight'
 		$heavyweights.push(f)
+	elsif division == "Women's Strawweight"
+		$womenstrawweights.push(f)
+	elsif division == "Women's Flyweight"
+		$womenflyweights.push(f)
+	elsif division == "Women's Bantamweight"
+		$womenbantamweights.push(f)
+	elsif division == "Women's Featherweight"
+		$womenfeatherweights.push(f)
+	end
+
+
+end
+
+def createTheFights(division_text, division)
+	$i = 0
+	while $i < 16  do
+		j=1
+		while j < 9 do
+			if $i + j < 16
+				f = Fight.where("fighter_one_id = "+division[$i].id.to_s + " and fighter_two_id = " + division[$i+j].id.to_s + "or fighter_one_id = "+division[$i+j].id.to_s + "and fighter_two_id = " + division[$i].id.to_s).first
+				#puts 'f: ' + f.to_s
+				if f != nil
+					f.fighter_one_id = division[$i].id
+					f.fighter_two_id = division[$i+j].id
+					f.division = division_text
+					f.save
+				else
+					Fight.create(division: division_text, upvotes: 0, fighter_one_id: division[$i].id, fighter_two_id: division[$i+j].id)					
+				end
+				
+			end
+			j += 1
+		end
+   		$i +=1
+	end
+
+end
+
+def createTheSuperFight(champion_one, champion_two)
+	f = Fight.where("fighter_one_id = "+champion_one.id.to_s + " and fighter_two_id = " + champion_two.id.to_s + "or fighter_one_id = "+champion_two.id.to_s + "and fighter_two_id = " + champion_one.id.to_s).first
+
+	if f != nil
+		f.fighter_one_id = champion_one.id
+		f.fighter_two_id = champion_two.id
+		f.division = 'Superfight'
+		f.save
+	else
+		Fight.create(division: "Superfight", upvotes: 0, fighter_one_id: champion_one.id, fighter_two_id: champion_two.id)					
 	end
 
 
@@ -79,31 +149,65 @@ end
 
 def createFights
 	puts 'Flyweights:' + $flyweights.to_s
-	$i = 0
-	while $i < 15  do
-		if $i + 1 < 15
-			Fight.create(division: 'Flyweight', upvotes: 0, fighter_one_id: $flyweights[$i].id, fighter_two_id: $flyweights[$i+1].id)
-		end
-		if $i + 2 < 15
-			Fight.create(division: 'Flyweight', upvotes: 0, fighter_one_id: $flyweights[$i].id, fighter_two_id: $flyweights[$i+2].id)
-		end
-		if $i + 3 < 15
-			Fight.create(division: 'Flyweight', upvotes: 0, fighter_one_id: $flyweights[$i].id, fighter_two_id: $flyweights[$i+3].id)
-		end
-		if $i + 4 < 15
-			Fight.create(division: 'Flyweight', upvotes: 0, fighter_one_id: $flyweights[$i].id, fighter_two_id: $flyweights[$i+4].id)
-		end
-   	$i +=1
-	end
+	createTheFights('Flyweight', $flyweights)
+	createTheSuperFight($flyweights[0], $bantamweights[0])
 
-	#puts 'Bantamweights:' + $bantamweights.to_s
-	#puts 'Featherweights:' + $featherweights.to_s
-	#puts 'Lightweights:' + $lightweights.to_s
-	#puts 'Welterweights:' + $welterweights.to_s
-	#puts 'Middleweights:' + $middleweights.to_s
-	#puts 'Light Heavyweights:' + $lightheavyweights.to_s
-	#puts 'Heavyweights:' + $heavyweights.to_s
+	puts 'Bantamweights:' + $bantamweights.to_s
+	createTheFights('Bantamweight', $bantamweights)
+	createTheSuperFight($bantamweights[0], $featherweights[0])
+
+
+	puts 'Featherweights:' + $featherweights.to_s
+	createTheFights('Featherweight', $featherweights)
+	createTheSuperFight($featherweights[0], $lightweights[0])
+
+
+	puts 'Lightweights:' + $lightweights.to_s
+	createTheFights('Lightweight', $lightweights)
+	createTheSuperFight($lightweights[0], $welterweights[0])
+
+	puts 'Welterweights:' + $welterweights.to_s
+	createTheFights('Welterweight', $welterweights)
+	createTheSuperFight($welterweights[0], $middleweights[0])
+
+	puts 'Middleweights:' + $middleweights.to_s
+	createTheFights('Middleweight', $middleweights)
+	createTheSuperFight($middleweights[0], $lightheavyweights[0])
+
+	puts 'Light Heavyweights:' + $lightheavyweights.to_s
+	createTheFights('Light Heavyweight', $lightheavyweights)
+	createTheSuperFight($lightheavyweights[0], $heavyweights[0])
+
+	puts 'Heavyweights:' + $heavyweights.to_s
+	createTheFights('Heavyweight', $heavyweights)
+
+	# WOMEN
+
+	puts 'Women Strawweights:' + $womenstrawweights.to_s
+	createTheFights("Women's Strawweight", $womenstrawweights)
+	createTheSuperFight($womenstrawweights[0], $womenflyweights[0])
+
+
+	puts 'Women Flyweights:' + $womenflyweights.to_s
+	createTheFights("Women's Flyweight", $womenflyweights)
+	createTheSuperFight($womenflyweights[0], $womenbantamweights[0])
+
+
+	puts "Women's Bantamweights:" + $womenbantamweights.to_s
+	createTheFights("Women's Bantamweight", $womenbantamweights)
+
+	puts "Women's Featherweights:" + $womenfeatherweights.to_s
+		
+
+
 end
+
+
+fighters = Fighter.all
+fighters.each{|fighter|
+	fighter.is_updated = 'false'
+	fighter.save
+}
 
 puts "---------FIGHTERS---------"
 
@@ -141,7 +245,7 @@ divisions.each{|division|
 	division_rankings = division.css('.rankings-table').css('tr')
 	division_rankings.each{|division_rank|
 		rank = division_rank.css('.number-column').inner_text.strip
-		fighter_rank = '# '+ rank + ' ' + division_name
+		fighter_rank = '#'+ rank + ' ' + division_name
 		name = division_rank.css('.name-column').css('a').inner_text.strip
 		#puts name
 
@@ -155,3 +259,15 @@ divisions.each{|division|
 
 }
 createFights()
+
+# Now check the fighters where is_updated=false. That means they are no longer ranked in the top 15. Delete their fights and delete them.
+
+unranked_fighters = Fighter.where(is_updated: 'false')
+unranked_fighters.each{|f|
+	# Delete all their fights
+	fights = Fight.where("fighter_one_id = '"+f.id+"' OR fighter_two_id = '"+f.id+"'")
+	fight.each{|fi|
+		Fight.destroy(fi.id)
+	}
+	Fighter.destroy(f.id)
+}
